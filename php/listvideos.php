2ff9f7302176f13d371f92d9a120c25fec1e1483
tservi <jeantinguelyawais@gmail.com>
2010-11-05 10:15:37 +0000

    creating the code by iteration ant testing....

--- php/listvideos.php
+++ php/listvideos.php
@@ -17,69 +17,30 @@ $PATHTOREPOSITORY = 'repository' ;
     <body>
 <?php
 
-    function writeEntry( $entry )
-    {
-        $e = $entry . '' ;   // transtyping
-        if ( $e != '.' && $e != '..' && $e[ 0 ] != '.' )
-            {
-                echo "<li>" . $e  . "</li>\n"           ;
-            }
-    }
 
-    function parseDir ( $path )
-    {
-        chdir   ( $path )    ;   
-        echo "<!-- " . getcwd() . " -->\n"                  ;
-        $d = dir( '.' )                                     ;
-        echo "<ul>\n"                                       ;
-        echo "<li>"; var_dump ( $d ) ; echo "</li>"         ;
-        $entry = $d->read()                                 ;
-        echo "<li>"; var_dump ( $entry ) ; echo "</li>"         ;
-        /*
-        while (false !== $entry )
-        {
-           if( is_dir( $entry) )
-           {
-                $e = $entry . '' ;   // transtyping
-                if ( $e != '.' && $e != '..' && $e[ 0 ] != '.' )
-                {
-                   writeEntry( $entry )                     ;
-                   parseDir( './' . $entry )                        ;
-                }
-            }
-        }
-        */
-        echo "</ul>\n"                                  ;
-        $d->close()                                     ;      
-    }
-
-
+    // thank you http://www.codingforums.com/showthread.php?t=71882
     function getDirectory( $path = '.', $level = 0 ){
     
         $ignore = array( '.svn', '.', '..' );    
         $dh = @opendir( $path );
         
-        while( false !== ( $file = readdir( $dh ) ) ){
-        
-            if( !in_array( $file, $ignore ) ){
-                                
-                if( is_dir( "$path/$file" ) ){
-                
-                    echo "<li><s>$file</s></li>\n<ul>\n";
+        while( false !== ( $file = readdir( $dh ) ) )
+        {
+            if( !in_array( $file, $ignore ) )
+            {                       
+                if( is_dir( "$path/$file" ) )
+                {
+                    echo "<li><b>$file</b></li>\n<ul>\n";
                     getDirectory( "$path/$file", ($level+1) );
                     echo "</ul>\n" ;
-                } else {
-                
+                }
+                else
+                {
                     echo "<li>$file<li>";                
                 }
-            
             }
-        
         }
-        
-        closedir( $dh );
-        // Close the directory handle
-    
+        closedir( $dh );    
     } 
 
     //chdir   ( $PATHTOREPOSITORY )    ;   
