7376ad7fc8d8799d4765e405d3ad1a77c3a5dc4a
tservi <jeantinguelyawais@gmail.com>
2010-11-05 09:01:36 +0000

    new version...

--- php/listvideos.php
+++ php/listvideos.php
@@ -1,6 +1,13 @@
 <?php
 
+/******************************************************************************
+  @title : edovideo
+  @author : Jean Tinguely Awais aka t-servi.com
+  @category : php script
+  @version : 0.1
+******************************************************************************/
 
+$PATHTOREPOSITORY = 'repository' ;
 
 
 ?>
