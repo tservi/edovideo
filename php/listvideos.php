6011354ac50bb230f2bbfd70c222605cdd42bfb9
tservi <jeantinguelyawais@gmail.com>
2010-11-05 10:12:58 +0000

    creating the code by iteration ant testing....

--- php/listvideos.php
+++ php/listvideos.php
@@ -53,11 +53,41 @@ $PATHTOREPOSITORY = 'repository' ;
         $d->close()                                     ;      
     }
 
+
+    function getDirectory( $path = '.', $level = 0 ){
+    
+        $ignore = array( '.svn', '.', '..' );    
+        $dh = @opendir( $path );
+        
+        while( false !== ( $file = readdir( $dh ) ) ){
+        
+            if( !in_array( $file, $ignore ) ){
+                                
+                if( is_dir( "$path/$file" ) ){
+                
+                    echo "<li><s>$file</s></li>\n<ul>\n";
+                    getDirectory( "$path/$file", ($level+1) );
+                    echo "</ul>\n" ;
+                } else {
+                
+                    echo "<li>$file<li>";                
+                }
+            
+            }
+        
+        }
+        
+        closedir( $dh );
+        // Close the directory handle
+    
+    } 
+
     //chdir   ( $PATHTOREPOSITORY )    ;   
     //echo "<!-- " . getcwd() . " -->\n"                  ;
     //echo "Pointeur : " . $d->handle . "\n";
     //echo "Chemin : " . $d->path . "\n";
-    parseDir( $PATHTOREPOSITORY )                                      ;    
+    //parseDir( $PATHTOREPOSITORY )                                      ;    
+    getDirectory ( $PATHTOREPOSITORY )                                      ;    
 ?>
     </body>
 </html>
